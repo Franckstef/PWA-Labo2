be37a91ae483ff427fd45673de1d7f6cce22e766
Franck Stefani <franckstefani1@gmail.com>
2022-02-01 18:09:17 -0500

    Restart

---
+++ sw.js
@@ -0,0 +1,42 @@
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installation en cours de v1');
+    event.waitUntil(
+      caches.open('v1').then(function(cache) {
+        console.log('[Service Worker] Mise en cache globale: app shell et contenu et tout et tout');
+        return cache.addAll([
+            'index.html',
+            'index.css',
+            'index.js',
+            'icones/favicon.ico',
+            'icones/avion.png',
+            'icones/avionpapier.png',
+            '.cones/icon-192x192.png',
+            'icones/icon-256x256.png',
+            'icones/icon-384x384.png',
+            'icones/icon-512x512.png',
+            'bootstrap-5.1.3-dist/css/bootstrap.min.css',
+            'icons-1.7.2/font/bootstrap-icons.css',
+            'bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
+            'images/IMG_3578.JPG',
+            'images/IMG_3836.JPG',
+            'images/IMG_3976.JPG'
+        ]);
+      })
+    );
+  });
+
+  self.addEventListener('fetch', function(event) {
+    console.log("Fetching ..." + event.request.url);
+    event.respondWith(caches.match(event.request).then((response) => {
+        if (response !== undefined) {
+            return response;
+        } else {
+            console.log("Fetching from fetch ..." + event.request.url);
+            return fetch(event.request);
+        }
+    }))
+ });
+
+  self.addEventListener('activate', (e) => {
+  console.log('Service worker activé, all good !');
+});
